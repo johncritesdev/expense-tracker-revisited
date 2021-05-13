19dc50a0e3148a44f0de0a5df4e4337733ca4c91
John Crites <johncritesdev@gmail.com>
2021-05-12 18:38:46 -0700

    38

--- src/App.js
+++ src/App.js
@@ -30,22 +30,22 @@ function App() {
         item={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         item={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         item={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         item={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
