45707c03cbd5c0e1401a9a707529702824524a81
John Crites <johncritesdev@gmail.com>
2021-06-11 11:00:04 -0700

    connect filter with list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,11 +11,15 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses = props.item.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.item.map(expense => <ExpenseItem
+        {filterExpenses.map(expense => <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
