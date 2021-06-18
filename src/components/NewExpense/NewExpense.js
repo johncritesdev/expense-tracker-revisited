5b4ec8ec160c935853a7328ddb45f127bcda665c
John Crites <johncritesdev@gmail.com>
2021-06-17 20:54:51 -0700

    add two buttons and conditions

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const expenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onSaveExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startExpenseHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={expenseDataHandler} />
+      {!isEditing && <button onClick={startExpenseHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={expenseDataHandler} onCancel={stopEditingHandler} />}
     </div>
   );
 }
